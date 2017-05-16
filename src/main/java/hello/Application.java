2a6d1e87299a246199bbf778c65d70c12a2fea8b
server <server@longdeMacBook-Pro.local>
2017-05-16 10:51:54 +0800

    Spring-boot 热部署

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -16,17 +16,17 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx =  SpringApplication.run(Application.class, args);
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-
-        try {
-            PushManagerInstance.getInstance().sendMessage();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        String[] beanNames = ctx.getBeanDefinitionNames();
+//        Arrays.sort(beanNames);
+//        for (String beanName : beanNames) {
+//            System.out.println(beanName);
+//        }
+//
+//        try {
+//            PushManagerInstance.getInstance().sendMessage();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
--- src/main/java/hello/SecondController.java
+++ src/main/java/hello/SecondController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecondController {
     @RequestMapping("/second")
     public String second() {
-        return "hello";
+        return "hell11o world";
     }
 }
